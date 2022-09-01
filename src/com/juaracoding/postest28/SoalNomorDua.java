bac90be3607b46c5de1819f03c9e1b648c495b3c
Dhanna Riyantka Putra <Dhanna217203@gmail.com>
2022-09-02 01:43:39 +0700

    PostTest28

--- src/com/juaracoding/postest28/SoalNomorDua.java
+++ src/com/juaracoding/postest28/SoalNomorDua.java
@@ -6,28 +6,25 @@ public class SoalNomorDua {
 		int intloop = 0;
 		int c = 10;
 		int b = 0;
-		for(int a= 0;a<=5;a++) {
+		for (int a = 0; a <= 5; a++) {
 			intloop++;
 
-			if(intloop <=0) {
-			}
-			else {
-				for (int i = b ;i<=c;i++) {					
-						if( i%2==0) {
+			if (intloop <= 0) {
+			} else {
+				for (int i = b; i <= c; i++) {
+					if (i % 2 == 0) {
 						System.out.print("_");
-						}
-						else {
-							System.out.print(i);
-						}
-				}	
-		    }
-		if( c==5 && b==5 ){
-		}
-		else {
-			c--;
-			b++;
-		}
-		System.out.println("");
+					} else {
+						System.out.print(i);
+					}
+				}
+			}
+			if (c == 5 && b == 5) {
+			} else {
+				c--;
+				b++;
+			}
+			System.out.println("");
 		}
 	}
 
