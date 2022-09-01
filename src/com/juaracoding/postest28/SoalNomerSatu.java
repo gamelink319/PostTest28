491af040c2e7596063ae38880be51aad6073612a
Dhanna Riyantka Putra <Dhanna217203@gmail.com>
2022-09-02 01:42:15 +0700

    PostTest28

---
+++ src/com/juaracoding/postest28/SoalNomerSatu.java
@@ -0,0 +1,21 @@
+package com.juaracoding.postest28;
+
+public class SoalNomerSatu {
+	    public static void main(String args[]) {
+	 	int intloop = 0;
+		for (int i =0;i<100;i++) {
+			intloop++ ;
+			for (int x =0;x<1;x++) {
+			    if(intloop%3==0 || intloop%5==0) {
+			        System.out.println("Bizzbuzz");
+		    	}
+		    	if(intloop%3==0) {
+			        System.out.println("Bizz");
+		    	}
+		    	if(intloop%5==0) {
+			        System.out.println("buzz");
+		    	}
+		    }
+		}
+	    }
+	}
---
+++ src/com/juaracoding/postest28/SoalNomorDua.java
@@ -0,0 +1,34 @@
+package com.juaracoding.postest28;
+
+public class SoalNomorDua {
+
+	public static void main(String[] args) {
+		int intloop = 0;
+		int c = 10;
+		int b = 0;
+		for(int a= 0;a<=5;a++) {
+			intloop++;
+
+			if(intloop <=0) {
+			}
+			else {
+				for (int i = b ;i<=c;i++) {					
+						if( i%2==0) {
+						System.out.print("_");
+						}
+						else {
+							System.out.print(i);
+						}
+				}	
+		    }
+		if( c==5 && b==5 ){
+		}
+		else {
+			c--;
+			b++;
+		}
+		System.out.println("");
+		}
+	}
+
+}
